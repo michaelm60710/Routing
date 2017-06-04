60062f69546099caa70590f323bf5413b1d71877
michaelm60710 <michaelm60710@gmail.com>
2017-06-04 14:39:28 +0800

    Revert "not good"
    This reverts commit 8ebdab19110aaa9168128915210d3752dba801ae.

--- layer.cpp
+++ layer.cpp
@@ -15,12 +15,6 @@ bool Sort_Shape(Shape* a, Shape* b){
 	return a->coords->x1 < b->coords->x1;
 }
 
-bool
-sort_func_x1(Shape* S1, Shape* S2){
-	if(S1->coords->x1 < S2->coords->x1) return true;
-	else if(S1->coords->x1 == S2->coords->x1) return (S1->coords->y1 < S2->coords->y1);
-	else return false;
-}
 
 
 void Layer::SpanningGraphConstruct(){
@@ -39,41 +33,41 @@ void Layer::SpanningGraphConstruct(){
 	
 	//#1.1 construct cluster
     clustering_shape();
+    //test
 
 	//for(MS_it it = X_msort_shape.begin();it!=X_msort_shape.end();++it)
 	
 
 }
 
-
+bool
+sort_func_x1(Shape* S1, Shape* S2){
+	if(S1->coords->x1 < S2->coords->x1) return true;
+	else if(S1->coords->x1 == S2->coords->x1) return (S1->coords->y1 < S2->coords->y1);
+	else return false;
+}
 
 void
 Layer::clustering_shape(){
 	cout<<"Clustering..."<<endl;
-	
-    for(size_t s = 0; s < all_shape_vec.size(); s++){
+	for(size_t s = 0; s < all_shape_vec.size(); s++){
 		all_shape_vec[s]->clu = NULL;
 		sort_by_x1.push_back(all_shape_vec[s]);
 	}
     sort(sort_by_x1.begin(), sort_by_x1.end(), sort_func_x1);
+    /*cout<<"layer:"<<endl;
+    cout<<"sort by x1:"<<endl;
+    for(size_t s = 0; s < sort_by_x1.size(); s++)
+    	cout<<sort_by_x1[s]->coords->x1<<", "<<sort_by_x1[s]->coords->y1<<endl;*/
     
-    //### 1. constuct overlap graph
-    for(size_t s = 0; s < sort_by_x1.size(); s++){
-        find_overlap(sort_by_x1[s], s+1);
-    }
-
-    //### 2. constuct cluster
     for(size_t s = 0; s < sort_by_x1.size(); s++){
         if(sort_by_x1[s]->clu == NULL){
             Cluster* C = new Cluster(sort_by_x1[s]);
+            sort_by_x1[s]->clu = C;
+            build_clu(sort_by_x1[s], C, s+1);
             all_cluster.push_back(C);
-            //BFS find the connected overlap graph
-
-            
         }
     }
-
-
     size_t count_O = 0;
     size_t count_S = 0;
     for(size_t i = 0; i < all_cluster.size(); i++){
@@ -82,17 +76,15 @@ Layer::clustering_shape(){
     	//cout<<S->coords->x1<<", "<<S->coords->y1<<"  "<<all_cluster[i]->shape_list.size()<<endl;
     	if(S->Shape_type == RSHAPE)count_S ++;
     	else count_O ++;
-    	it_shape it = all_cluster[i]->shape_list.begin();
+    	/*it_shape it = all_cluster[i]->shape_list.begin();
     	cout<<"elements:"<<endl;
     	for(; it != all_cluster[i]->shape_list.end(); it++)
     		cout<<(*it)->coords->x1<<", "<<(*it)->coords->y1<<"; "
-    	    <<(*it)->coords->x2<<", "<<(*it)->coords->y2<<endl;
+    	    <<(*it)->coords->x2<<", "<<(*it)->coords->y2<<endl;*/
     }
-
-
-    //cout<<"total cluster: "<<all_cluster.size();
-    //cout<<" ( # of Obstacle cluster = "<<count_O;
-    //cout<<", # of Rshape cluster = "<<count_S<<" )"<<endl;
+    cout<<"total cluster: "<<all_cluster.size();
+    cout<<" ( # of Obstacle cluster = "<<count_O;
+    cout<<", # of Rshape cluster = "<<count_S<<" )"<<endl;
 
 
 }
@@ -125,28 +117,9 @@ Layer::overlap(Shape* S1, Shape* S2){
 }
 
 
-void 
-Layer::find_overlap(Shape* S, int idx){
-    for(size_t s = idx; s < sort_by_x1.size(); s++){
-        //while(sort_by_x1[s]->coords->x2 < S->coords->x1)s++; //??
-        if(sort_by_x1[s]->coords->x1 > S->coords->x2) break;
-        if(overlap(S, sort_by_x1[s])){
-            S->Overlaps.push_back(sort_by_x1[s]);  
-            sort_by_x1[s]->Overlaps.push_back(S);  
-        }   
-        }
-
-}
-
-
-void 
-Layer::BFS_overlap_graph(Shape *S, Cluster* C){
-    if(S->clu!=NULL) return;
 
 
 
-
-}
 
 
 
